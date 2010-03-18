0b4d6007898fa6c1175f9743da58a2b23c18ce84
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2010-03-18 17:21:27 +0100

    load jQuery, Google Feed API and provide custom Application.FeedManager

---
+++ site/javascripts/application/feed-manager.js
@@ -0,0 +1,30 @@
+var Application = Application || {};
+
+Application.FeedManager = function(givenConfig) {
+  var defaultConfig = {
+    // available options
+    url: null,
+    renderTo: null,
+    title: null,
+    linkTarget: google.feeds.LINK_TARGET_BLANK,
+    numEntries: 15
+  };
+  
+  if (givenConfig) {
+    $.extend(defaultConfig, givenConfig);
+  }
+  
+  return {
+    // draw feedControl into element specified by renderTo
+    // an optional config will override both the defaultConfig and givenConfig
+    render: function(config) {
+      config = config || {};
+      var feedControl = new google.feeds.FeedControl();
+
+      feedControl.setNumEntries(config.numEntries || defaultConfig.numEntries);
+      feedControl.setLinkTarget(config.linkTarget || defaultConfig.linkTarget);
+      feedControl.addFeed(config.url || defaultConfig.url, config.title || defaultConfig.title);
+      feedControl.draw(config.renderTo || defaultConfig.renderTo);
+    }
+  };
+};
