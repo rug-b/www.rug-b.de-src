47be6fbb9238e960c36c80ef75a853e8b40efd15
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2010-07-20 22:20:43 +0200

    fix stylesheets path in compass config

--- config/compass.rb
+++ config/compass.rb
@@ -1,12 +1,9 @@
-ROOT = File.join(File.dirname(__FILE__), '/')
-puts "Site root is: " + File.expand_path(ROOT)
-
 require "redgreengrid" # version > 0.3.2
  
 output_style = :expanded
-project_path = ROOT               # must be set for Compass to work 
-sass_dir     = "stylesheets"  # dir containing Sass / Compass source files
-http_path    = "/"                # root when deployed
+project_path = File.join(File.dirname(__FILE__), '/') # must be set for Compass to work 
+sass_dir     = "../src/stylesheets"  # dir containing Sass / Compass source files
+http_path    = "/"                   # root when deployed
 css_dir      = "../site/stylesheets" # final CSS
 images_dir   = "../site/images"      # final images
 http_images_path = "/images"
